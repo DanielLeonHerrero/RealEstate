16b6a43874445caf544308beba733cfeea2bacfd
Daniel Leon Herrero <danielleon157@outlook.com>
2023-11-22 18:33:00 +0100

    Dark mode auto preference by user in general SO setting

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function darkmode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    }
+
     botonDarkMode.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    prefiereDarkMode.addEventListener('change', () => {
         document.body.classList.toggle('dark-mode');
     });
 }
